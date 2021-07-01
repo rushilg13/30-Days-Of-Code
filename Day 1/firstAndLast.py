537c7edaef88e5eb66320822384445744a275a16
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-01 14:27:25 +0530

    Added Approach

--- Day 1/firstAndLast.py
+++ Day 1/firstAndLast.py
@@ -13,3 +13,22 @@ while(arr[front] != arr[end]):
         end -=1
 print(front, end)
 
+# Approach - 2 - O(log(n)) - INCOMPLETE
+arr = [1, 3, 5, 5, 5, 5, 67, 123, 125]
+target = 5
+low = 0
+res = [-1, -1]
+high = len(arr)
+while(low <= high):
+    mid = int((low+high)//2)
+    if(arr[mid] < target):
+        low = mid + 1
+    elif(arr[mid] == target):
+        high = mid
+        res[0] = mid
+        res[1] = mid
+    else:
+        high = mid
+    if res[0] == -1:
+        print(res)
+        break
