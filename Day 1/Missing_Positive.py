7a14fd38048859de5f6e3c2644dc890b63a30bfb
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-02 23:43:47 +0530

    fix

--- Day 1/Missing_Positive.py
+++ Day 1/Missing_Positive.py
@@ -1,21 +1,35 @@
 # Given an unsorted integer array nums, find the smallest missing positive integer.
 
-# Approach - 1 - O(n)
-arr = [7,8,9,11,12]
-sum1 = 0
-sum2 = 0
-mini = min(arr)
-maxi = max(arr)
-for i in range(mini, maxi +1):
-    sum1 += i
-for i in arr:
-    sum2 += i
-print(sum1-sum2)
+# Approach - 1 - O(n^2)
+arr = [1, 2, 3, 4, 5, 7, 8, 9, 11, 12]
+i = 1
+while(True):
+    if i not in arr:
+        print(i)
+        break
+    else:
+        i += 1
 
-
-# Approach - 2 - O(nlog(n))
+# Approach - 2 - O(nlog(n)) + O(n) = O(nlog(n))
 arr = [5, 4, 1, 2, 6]
 arr.sort()
-for i in range(len(arr)-1):
-    if (arr[i] + 1 != arr[i+1]):
-        print(arr[i]+1)
+if arr[0] != 1:
+    print(1)
+else:
+    for i in range(len(arr)-1):
+        if(arr[i]+1 != arr[i+1]):
+            print(arr[i]+1)
+
+# Approach - 3 - Hashing - O(n) time and space
+arr = [1, 2, 3, 4, 5, 7, 8, 9, 11, 12]
+dic = {}
+for i in range(len(arr)):
+    dic[arr[i]] = i
+i = 1
+while(True):
+    if i not in dic:
+        print(i)
+        break
+    else:
+        i += 1
+
