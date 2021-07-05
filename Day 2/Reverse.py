3659327556e4f71d962166cbf949522c5182048c
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-05 21:53:37 +0530

    solved

--- Day 2/Reverse.py
+++ Day 2/Reverse.py
@@ -31,10 +31,38 @@ class LinkedList:
             print("NULL")
     
     def reverse(self):
-        pass
+        if self.start == None:
+            print("Empty!")
+            return
+        if self.start.next == None:
+            print(self.start.value, "-> NULL")
+        if self.start.next.next == None:
+            print(self.start.next.value, "->" , self.start.value, "NULL")
+        else:
+            t1 = self.start
+            t2 = self.start
+            t3 = self.start.next
+            while t3.next != None:
+                if t1 == self.start:
+                    t1.next = None
+                    t2 = t3
+                    t3 = t3.next
+                    t2.next = t1
+                    t1 = t2
+                else:
+                    t2 = t3
+                    t3 = t3.next
+                    t2.next = t1
+                    t1 = t2
+            t3.next = t2
+            while t3 != None:
+                print(t3.value, end = " -> ")
+                t3 = t3.next
+            print("NULL")
+
 
 List1 = LinkedList()
-l1 = [1,2,3,4,5,6,7,8]
+l1 = [1,2,3,4,5,6]
 for i in l1:
     List1.append(i)
 List1.view()
