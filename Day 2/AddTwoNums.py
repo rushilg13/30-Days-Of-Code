bf2761979afcbefa8fffd6599b8878ecef24d4ea
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-05 20:34:48 +0530

    fixed

--- Day 2/AddTwoNums.py
+++ Day 2/AddTwoNums.py
@@ -2,9 +2,6 @@
 # The digits are stored in reverse order, and each of their nodes contains a single digit. 
 # Add the two numbers and return the sum as a linked list.
 
-from typing import List
-
-
 class Node:
     def __init__(self, value):
         self.value = value
