2455425a20da1796b2be24a96590bf3e5a3708d6
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-06 22:11:00 +0530

    solved

---
+++ Day 3/Fibonacci.py
@@ -0,0 +1,16 @@
+# Fibonacci Series
+
+# Approach - 1 - Using number of terms
+def fib(a, b, n):
+    if n == 0:
+        return 0
+    else:
+        print(a, b, end=' ')
+        a = a + b
+        b = b + a
+        n -= 2
+        return fib(a, b, n)
+
+fib(0, 1, 20)
+
+# Approach - 2 - Using last term
