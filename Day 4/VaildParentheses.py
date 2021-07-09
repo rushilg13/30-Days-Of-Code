1dbb5737a224c69df0b3744af56f23617085f48a
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-09 09:18:30 +0530

    solved

--- Day 4/VaildParentheses.py
+++ Day 4/VaildParentheses.py
@@ -2,3 +2,23 @@
 # An input string is valid if:
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
+
+s = "[]"
+arr = []
+if len(s) % 2 != 0:
+    print(False)
+    exit()
+for i in s:
+    if (i == "[") or (i == "{") or (i == "("):
+        arr.append(i)
+    elif (i == "]") or (i == "}") or (i == ")"):
+        if arr[-1] == "[" and i == "]":
+            arr.pop()
+        elif arr[-1] == "{" and i == "}":
+            arr.pop()
+        elif arr[-1] == "(" and i == ")":
+            arr.pop()
+    if len(arr) == 0:
+        print(True)
+    else:
+        print(False)
