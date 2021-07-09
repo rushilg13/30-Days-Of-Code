739aae375b11ad8823290dc63d83473d21619cfc
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-09 14:46:35 +0530

    solved

--- Day 4/QueueUsingStacks.py
+++ Day 4/QueueUsingStacks.py
@@ -1,2 +1,14 @@
 # Implement a Queue using 2 Stacks
 
+arr1 = []
+arr2 = []
+arr1.append(5)
+arr1.append(4)
+arr1.append(3)
+arr1.append(2)
+arr1.append(1)
+arr1.append(0)
+for i in range(len(arr1)):
+    a = arr1.pop()
+    arr2.append(a)
+print(arr2)
