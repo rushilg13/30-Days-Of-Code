43ed58416ea29a3f141f77f25ab8d03ae55338de
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-11 07:36:08 +0530

    solved

--- Day 4/MinAddParentheses.py
+++ Day 4/MinAddParentheses.py
@@ -1,2 +1,14 @@
 # Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
 
+s = "()))(("
+arr = []
+count = 0
+for i in s:
+    if (i == "("):
+        arr.append(i)
+    else:
+        if arr != []:
+            arr.pop()
+        else:
+            count += 1
+print(count + len(arr))
