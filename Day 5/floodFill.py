fd0cd29707fc94d667d7b1777a950c07ba33baf0
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-11 17:03:33 +0530

    Added

---
+++ Day 5/floodFill.py
@@ -0,0 +1,5 @@
+# o perform a flood fill, consider the starting pixel, 
+# plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+#  plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. 
+# Replace the color of all of the aforementioned pixels with newColor.
+
