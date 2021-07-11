0f881a3a15c890b37dba7ec87103ff601587ad9d
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-11 08:35:54 +0530

    solved

--- Day 4/TaskScheduler.py
+++ Day 4/TaskScheduler.py
@@ -5,4 +5,18 @@
 # that is that there must be at least n units of time between any two same tasks.
 # Return the least number of units of times that the CPU will take to finish all the given tasks.
 
-tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"]
+tasks = ["A","A","A","B","B"]
+dic = {}
+n = 2
+for i in tasks:
+    dic[i] = tasks.count(i)
+count_arr = list(dic.values())
+count_arr.sort(reverse=True)
+max_num = count_arr[0]
+i = 0
+counter = 0
+while i < len(count_arr) and count_arr[i] == max_num:
+    counter += 1
+    i += 1
+ret = (max_num - 1) * (n + 1) + counter
+print(max(ret, len(tasks)))
