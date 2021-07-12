c8daca935a68d4d04d4302de8a4acf6cf8bb563b
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-12 14:37:35 +0530

    added

--- Day 5/floodFill.py
+++ Day 5/floodFill.py
@@ -3,3 +3,28 @@
 #  plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. 
 # Replace the color of all of the aforementioned pixels with newColor.
 
+class Queue:
+    def __init__(self) -> None:
+        self.arr = []
+    
+    def enQueue(self, val):
+        self.arr.append(val)
+    
+    def deQueue(self):
+        pop = self.arr[0]
+        self.arr.remove(self.arr[0])
+        return pop
+
+    def isEmpty(self):
+        if self.arr == []:
+            return True
+        else:
+            return False
+
+image =[[1,1,1],
+        [1,1,0],
+        [1,0,1]]
+sr = 1
+sc = 1
+newColor = 2
+
